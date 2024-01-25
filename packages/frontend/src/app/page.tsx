992cc50db06ae94c8fa597d497974ce5ce7ec314
Max Draginich <draginichdev@gmail.com>
2024-01-25 13:08:16 +0200

    chore: updated

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react';
-import Movies from '@/pages/movies';
+import Movies from '@/components';
 
 export default function Home() {
   return (
--- packages/frontend/src/pages/movies/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { getMovies } from '../api/movies';
-
-export default async function Movies() {
-  const { results } = await getMovies();
-
-  return <h1 className="flex space-x-4">{results.map((movie: any) => movie.backdrop_path)}</h1>;
-}
